1378b285e51372fdce000d860565c04d285f298a
auntyellow <auntyellow>
2017-04-06 17:19:35 +0800

    add test

--- 24.js
+++ 24.js
@@ -2,7 +2,11 @@ function solve1(expressions, na, nb, nc, nd, na1, nb1, na2, na3) {
   var solutions = [];
   for (var i = 0; i < expressions.length; i ++) {
     var value = expressions[i];
+    value = value.split("a'''").join(na3);
+    value = value.split("a''").join(na2);
+    value = value.split("a'").join(na1);
     value = value.split("a").join(na);
+    value = value.split("b'").join(nb1);
     value = value.split("b").join(nb);
     value = value.split("c").join(nc);
     value = value.split("d").join(nd);
@@ -26,10 +30,16 @@ function solve(ns) {
         return solve1(EXP_AAAA, n0);
       }
       // n0 = n1 = n2 < n3
+      if (n3 - n0 == 1) {
+        return solve1(EXP_AAA1, n0, 0, 0, 0, n3);
+      }
       return solve1(EXP_AAAB, n0, n3);
     }
     if (n2 == n3) {
       // n0 = n1 < n2 = n3
+      if (n2 - n1 == 1) {
+        return solve1(EXP_AA11, n0, 0, 0, 0, n2);
+      }
       return solve1(EXP_AABB, n0, n2);
     }
     // n0 = n1 < n2 < n3
